e313b61d04e6f8ebda8457cd15ae9835ec97ee37
the-priyankaa <priyanka.biswas3690@gmail.com>
2026-06-19 21:40:23 +0530

    fix: correct game loop condition and improve guess handling logic

--- HangmanGame.py
+++ HangmanGame.py
@@ -19,9 +19,30 @@ guessed_letters = set()
 
 #use function 
 def start_game():
+    # Declare as global
+    global wrong_guesses, display, guessed_letters  
+
     # Use loop
-    while wrong_guesses <= max_wrong:
+    while wrong_guesses < max_wrong:
         guess = input("Guess a letter: ").lower()
 
+        #check it is a right guess or not
+        w = list(word)
+        
+        if guess in w:
+            print("Right guess")
+
+            # Update display with correct letters
+            for j in range(len(w)):
+                if w[j] == guess:
+                    display[j] = guess
+        elif guess == guessed_letters:
+            print("OOPS! YOU ALREADY ENTER THIS!")            
+        else:
+            print("OPPS! WRONG GUESS")
+            wrong_guesses = wrong_guesses +1
         # Add to guessed letters
         guessed_letters.add(guess) 
+
+start_game()
+print(word)
